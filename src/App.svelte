0b6841bf0065d3c006342e4ee182d2288bd9187d
Nawasan <arikato110011@gmail.com>
2022-10-16 18:22:20 +0700

    change n to N

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,7 @@
   class="min-h-screen bg-purple-50 border-4 border-purple-700 flex justify-center items-center box-border flex-col"
 >
   <div class="text-3xl sm:text-6xl text-slate-800 font-medium">
-    Hello!, I'm <span class="text-purple-700 use-font font-bold">nawasan</span><span
+    Hello!, I'm <span class="text-purple-700 use-font font-bold">Nawasan</span><span
       class="inline-block hover:rotate-45 duration-300 ease-in-out transition"
       >👋</span
     >
