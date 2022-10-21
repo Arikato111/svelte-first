5013af657bbbe774c652192b9e51ab1ffef2df41
Nawasan <arikato110011@gmail.com>
2022-10-21 17:48:12 +0700

    fixed back button can't click

--- src/components/CertificateShow.svelte
+++ src/components/CertificateShow.svelte
@@ -6,11 +6,14 @@
   let domain = "https://hack1.hackathailand.com";
 </script>
 
-<div
-  class=" lg:hover:scale-125 transition-all duration-500 inline-block"
->
-  <a class="m-10 inline-block" target="_blank" rel="noreferrer" href={domain + link}>
-    <Atropos class="my-atropos inline-block">
+<div>
+  <a
+    class="m-10 inline-block"
+    target="_blank"
+    rel="noreferrer"
+    href={domain + link}
+  >
+    <Atropos class="my-atropos inline-block lg:hover:scale-125 transition-all duration-500">
       <img
         class="w-80 shadow-lg shadow-gray-400 rounded-lg border-2 border-purple-300"
         {src}
