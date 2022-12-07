457383ec7e5e7f120b3cbe5ae5be679e8d06fbdc
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2022-12-07 21:49:03 +0700

    fixed shadow of certificates

--- src/components/CertificateShow.svelte
+++ src/components/CertificateShow.svelte
@@ -8,7 +8,7 @@
 <div class="m-10 inline-block">
   <span class="dark:text-slate-200 font-bold">{type}</span>
   <Atropos
-    class="my-atropos shadow-lg  hidden shadow-slate-400 dark:shadow-slate-600  lg:inline-block lg:hover:scale-125 transition-all duration-500"
+    class="my-atropos shadow-lg hover:shadow-none  hidden shadow-slate-400 dark:shadow-slate-600  lg:inline-block lg:hover:scale-125 transition-all duration-500"
   >
     <img
       class="w-80 hidden lg:inline-block rounded-lg border-2 border-purple-300"
