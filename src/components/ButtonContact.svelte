aabf5ab8cd7af85a235e5628b3ad4a8e09a2cef1
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-01-13 23:13:32 +0700

    remove border button

--- src/components/ButtonContact.svelte
+++ src/components/ButtonContact.svelte
@@ -8,7 +8,7 @@
   dark:bg-slate-900 text-white dark:text-slate-200
     fill-white  dark:fill-slate-200 flex justify-center 
     items-center rounded-lg border-2 py-1 px-3 font-medium
-     border-purple-400 dark:border-slate-200 
+     border-transparent dark:border-slate-200 
      dark:hover:bg-gray-700 m-1 hover:scale-105"
 >
   <span class="inline-block w-8 mx-1">
