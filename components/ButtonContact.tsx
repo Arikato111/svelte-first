963e528e61cb7438b85cc3d06ae392d896afecf3
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-05-22 13:49:58 +0700

    change styles button

--- components/ButtonContact.tsx
+++ components/ButtonContact.tsx
@@ -9,13 +9,13 @@ type ButtonContactInput = {
 const ButtonContact: FC<ButtonContactInput> = ({ title, Icon, Class }) => {
   return (
     <div
-      className={`${Class ?? ""} duration-200 bg-purple-400 hover:bg-purple-500
+      className={`${Class ?? ""} duration-300 bg-purple-500 hover:bg-purple-600
   dark:bg-slate-900 text-white dark:text-slate-200
     fill-white  dark:fill-slate-200 flex justify-center 
     items-center rounded-lg border-2 py-1 px-3 font-medium
      border-purple-400 dark:border-slate-200 
      dark:hover:bg-gray-700 m-1 hover:scale-105
-     shadow-md shadow-gray-300 dark:shadow-none`}
+     shadow-md hover:shadow-lg hover:shadow-purple-300 shadow-gray-300 dark:shadow-none`}
     >
       <span className="inline-block w-8 mx-1">
         <Icon />
