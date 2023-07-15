a57063782230147a64dd3d2bebde4430ecd9044b
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-07-15 12:32:05 +0700

    update: update dependencies; fixed: projects page change animation because it too slow;

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -17,27 +17,24 @@ const ProjectCard: FC<ProjectCardProps> = ({
 }) => {
   return (
     <div className="h-[370px] mb-5">
-      <div className="img-block sm:shadow-purple-200 sm:hover:shadow-purple-200 dark:hover:shadow-lg dark:shadow-md dark:shadow-slate-600 dark:hover:shadow-slate-800 bg-white p-1 dark:p-0 border sm:scale-95 sm:hover:scale-100 duration-500 relative inline-block  w-[320px] h-[370px] rounded-lg overflow-hidden sm:shadow sm:hover:shadow-lg dark:border-slate-200  dark:bg-slate-800">
-        <div className=" shadow-none border-purple-500 rounded-lg overflow-hidden">
+      <div className="img-block border-purple-500  sm:shadow-purple-200 sm:hover:shadow-purple-200 dark:hover:shadow-lg dark:shadow-md dark:shadow-slate-600 dark:hover:shadow-slate-800 bg-white p-1 border sm:scale-95 sm:hover:scale-100 duration-500 relative inline-block  w-[320px] h-[370px] rounded-lg overflow-hidden sm:shadow sm:hover:shadow-lg dark:border-slate-200  dark:bg-slate-800">
+        <div className=" shadow-none rounded-lg overflow-hidden">
           <div className="overflow-hidden">
             <img
-              className="img-hover sm:scale-125 w-full object-cover"
+              className="img-hover lg:scale-125 h-[200px] w-full object-cover"
               src={src}
               alt="project images"
             />
           </div>
-          <h3 className="text-heading text-xl py-1 text-zinc-800 dark:text-white">
-            {title}
+          <h3 className="text-xl py-1 text-zinc-800 dark:text-white">
+            <b>{title}</b>
           </h3>
           <div className="dark:text-white">
-            <div>{descript}</div>
-            <div className="descript-hide border-y-2 py-5 px-3 my-3 flex justify-center items-center text-center font-bold overflow-hidden">
-            {title} 
-            </div>
-            <div className="link-contact bottom-1 absolute my-1 w-full">
+            <div className="px-3">{descript}</div>
+            <div className="link-contact lg:h-0 lg:invisible bottom-1 absolute my-1 w-full">
               <a
                 target="_blank"
-                className="border-2 border-white shadow dark:shadow-none shadow-gray-400 hover:shadow-lg hover:shadow-gray-300 hover:scale-105 inline-block px-2 hover:px-5 py-1 bg-purple-600 hover:bg-purple-600/90  text-white rounded  duration-200 dark:border-white dark:bg-slate-800 dark:hover:bg-slate-700"
+                className="border-2 border-white shadow dark:shadow-none shadow-gray-400 hover:shadow-lg hover:shadow-gray-300 hover:scale-105 inline-block px-2 hover:px-5 py-1 bg-purple-600 hover:bg-purple-600/90  text-white rounded  duration-300 dark:border-white dark:bg-slate-800 dark:hover:bg-slate-700"
                 href={link}
               >
                 website
@@ -45,7 +42,7 @@ const ProjectCard: FC<ProjectCardProps> = ({
               {"  "}
               <a
                 target="_blank"
-                className="border-2 border-white shadow dark:shadow-none shadow-gray-400 hover:shadow-lg hover:shadow-gray-300 hover:scale-105 inline-block px-2 hover:px-5 py-1 bg-purple-600 hover:bg-purple-600/90  text-white rounded  duration-200 dark:border-white dark:bg-slate-800 dark:hover:bg-slate-700"
+                className="border-2 border-white shadow dark:shadow-none shadow-gray-400 hover:shadow-lg hover:shadow-gray-300 hover:scale-105 inline-block px-2 hover:px-5 py-1 bg-purple-600 hover:bg-purple-600/90  text-white rounded  duration-300 dark:border-white dark:bg-slate-800 dark:hover:bg-slate-700"
                 href={source}
               >
                 source code
