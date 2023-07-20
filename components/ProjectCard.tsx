9b536251e4bcdd482d347c6c567972cc178cd63b
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-07-20 21:01:58 +0700

    update styles; change images of projects preview;

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -16,25 +16,25 @@ const ProjectCard: FC<ProjectCardProps> = ({
   src,
 }) => {
   return (
-    <div className="h-[370px] mb-5">
-      <div className="img-block border-purple-500  sm:shadow-purple-200 sm:hover:shadow-purple-200 dark:hover:shadow-lg dark:shadow-md dark:shadow-slate-600 dark:hover:shadow-slate-800 bg-white p-1 border sm:scale-95 sm:hover:scale-100 duration-500 relative inline-block  w-[320px] h-[370px] rounded-lg overflow-hidden sm:shadow sm:hover:shadow-lg dark:border-slate-200  dark:bg-slate-800">
-        <div className=" shadow-none rounded-lg overflow-hidden">
-          <div className="overflow-hidden">
+    <div className="h-[390px] mb-5">
+      <div className="img-block p-1 duration-500 relative inline-block  w-[320px] h-[390px] overflow-hidden dark:border-slate-200">
+        <div className=" shadow-none overflow-hidden">
+          <div className="overflow-hidden rounded-md border border-purple-600 shadow-md shadow-gray-300 dark:shadow-gray-800">
             <img
-              className="img-hover lg:scale-125 h-[200px] w-full object-cover"
+              className="img-hover shadow-inner h-[200px] w-full object-cover"
               src={src}
               alt="project images"
             />
           </div>
-          <h3 className="text-xl py-1 text-zinc-800 dark:text-white">
+          <h3 className="text-xl py-3 text-zinc-800 dark:text-white">
             <b>{title}</b>
           </h3>
           <div className="dark:text-white">
             <div className="px-3">{descript}</div>
-            <div className="link-contact lg:h-0 lg:invisible bottom-1 absolute my-1 w-full">
+            <div className="link-contact lg:h-0 overflow-hidden duration-300 bottom-1 absolute my-1 w-full">
               <a
                 target="_blank"
-                className="border-2 border-white shadow dark:shadow-none shadow-gray-400 hover:shadow-lg hover:shadow-gray-300 hover:scale-105 inline-block px-2 hover:px-5 py-1 bg-purple-600 hover:bg-purple-600/90  text-white rounded  duration-300 dark:border-white dark:bg-slate-800 dark:hover:bg-slate-700"
+                className="border-2 border-white shadow dark:shadow-none shadow-gray-400 hover:shadow-lg hover:shadow-gray-300 hover:scale-105 inline-block px-2 hover:px-5 py-1 bg-purple-600 hover:bg-purple-600/90  text-white rounded-md  duration-300 dark:border-white dark:bg-slate-800 dark:hover:bg-slate-700"
                 href={link}
               >
                 website
@@ -42,7 +42,7 @@ const ProjectCard: FC<ProjectCardProps> = ({
               {"  "}
               <a
                 target="_blank"
-                className="border-2 border-white shadow dark:shadow-none shadow-gray-400 hover:shadow-lg hover:shadow-gray-300 hover:scale-105 inline-block px-2 hover:px-5 py-1 bg-purple-600 hover:bg-purple-600/90  text-white rounded  duration-300 dark:border-white dark:bg-slate-800 dark:hover:bg-slate-700"
+                className="border-2 border-white shadow dark:shadow-none shadow-gray-400 hover:shadow-lg hover:shadow-gray-300 hover:scale-105 inline-block px-2 hover:px-5 py-1 bg-purple-600 hover:bg-purple-600/90  text-white rounded-md  duration-300 dark:border-white dark:bg-slate-800 dark:hover:bg-slate-700"
                 href={source}
               >
                 source code
