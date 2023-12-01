d058d2e2e070c02761be0f27e465d5c4e7883ad4
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2023-12-01 20:19:28 +0700

    setup cache for images

--- next.config.js
+++ next.config.js
@@ -1,21 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async() => [
+  headers: async () => [
+    {
+      source: "/:all*(svg|jpg|png)",
+      locale: false,
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "public, max-age=2629743, must-revalidate",
+        },
+      ],
+    },
+  ],
+  rewrites: async () => [
     {
       source: "/api/info",
-      destination: "/info.json"
-    }
+      destination: "/info.json",
+    },
   ],
   reactStrictMode: true,
   images: {
     domains: [
-    "life-coach-quotes.vercel.app",
-    "what-to-read-nu.vercel.app",
-    "movie-random-react.vercel.app",
-    "next-food-random.vercel.app",
-
-    ]
-  }
+      "life-coach-quotes.vercel.app",
+      "what-to-read-nu.vercel.app",
+      "movie-random-react.vercel.app",
+      "next-food-random.vercel.app",
+    ],
+  },
 };
 
 module.exports = nextConfig;
