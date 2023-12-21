70bc66940be0f027fa50eccf053df03d3c9c38f7
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2023-12-21 16:19:18 +0700

    updatte projects display

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,17 +7,17 @@ export default function Projects() {
             <h3 className='font-bold text-5xl '>My Projects</h3>
             <h4 className='text-2xl'>about website, program and others</h4>
         </div>
-        <div className='grid grid-cols-2'>
-            {ProjectsData.map((project, idx) => idx < 6 && (
-                <div key={idx} className='project-card  my-7 px-5 flex items-center flex-col'>
-                    <div className='relative'>
-                        <div className="project-detail">{project.descript}</div>
-                        <img className='project-img w-full rounded-lg object-fill shadow' src={project.img} alt={project.title} width={300} height={200} title={project.title} />
+        <div>
+            {ProjectsData.map((project, idx) => idx < 3 && (
+                <div key={idx} className={`my-20 px-5 ${idx % 2 !== 0 ? "flex-row-reverse": "flex-row"} flex`}>
+                    <div className='px-3 relative flex-1'>
+                        <img className='w-full rounded-lg object-fill shadow' src={project.img} alt={project.title} width={300} height={200} title={project.title} />
                     </div>
-                    <div>
+                    <div className='flex-1 px-3'>
                         <a className='hover:underline' target='_blank' href={project.source}>
-                            <h4 className='my-3 font-bold'>{project.title}</h4>
+                            <h4 className='my-3 text-slate-800 text-3xl font-bold'>{project.title}</h4>
                         </a>
+                        <div>{project.descript}</div>
                     </div>
                 </div>
             ))}
