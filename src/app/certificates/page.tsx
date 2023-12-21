40023a8520da7cf7e0592c80762a3ad9dbbb33fb
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2023-12-21 19:15:15 +0700

    add certificates page

---
+++ src/app/certificates/page.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link"
+import CertificatesData from '../../data/CertificatesData.json'
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Certificates | Arikato111"
+}
+
+export default function CertifacatePage() {
+    if(CertificatesData[0].id == 0) CertificatesData.reverse()
+    return <div className='frame'>
+        <div className="box-lg">
+            <div className='my-10 text-center'>
+                <h3 className='font-bold text-5xl'>My Certificates</h3>
+                <h4 className='text-2xl'>from studying, competition and others</h4>
+            </div>
+            <div className='grid grid-cols-3 bg-gray-50'>
+                {CertificatesData.map((cer, idx) => (
+                    <div key={idx} className='my-7 px-5 flex items-center flex-col'>
+                        <img className='w-full rounded-lg object-fill shadow' src={cer.img.low} alt={cer.title} width={300} height={200} title={cer.type} />
+                        <div>
+                            <h4 className='my-3 font-bold'>{cer.title}</h4>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </div>
+}
