696ce2fa79506e38997afb48d20512173fc3df77
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2023-12-21 23:01:10 +0700

    fix: char

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,7 @@ export default function Banner() {
     return <div>
         <div className="flex h-[400px] items-center justify-center">
             <div>
-                <h1 className="text-4xl sm:text-5xl md:text-7xl font-bold text-center">Hello, I'm Arikato111</h1>
+                <h1 className="text-4xl sm:text-5xl md:text-7xl font-bold text-center">Hello, I&apos;m Arikato111</h1>
                 <h2 className="text-xl sm:text-2xl md:text-3xl font-medium ml-5">My real name is Nawasan Wisitsingkhon.</h2>
             </div>
         </div>
