75f0c744cf1706f22119ea766d55ae7a197f36e3
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2023-12-21 21:42:10 +0700

    add about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,14 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "About | Arikato111"
+}
+
 export default function About() {
     return <div className='frame'>
         <div className='my-10 text-center'>
-            <h3 className='font-bold text-5xl '>My Certificates</h3>
-            <h4 className='text-2xl'>from studying, competition and others</h4>
-        </div>
-        <div className='grid grid-cols-2'>
+            <h3 className='font-bold text-5xl '>About me</h3>
+            <h4 className='text-2xl'>I feel lazy, so I let this page empty.</h4>
         </div>
     </div>
 }
