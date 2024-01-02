29526de7bc12b92787048bd6670becfc6183eddf
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2024-01-02 17:20:36 +0700

    make: icon svg to icon ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children, }: { children: React.ReactNode })
   return (
     <html lang="en">
       <head>
-        <link rel="shortcut icon" href="/na.svg" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       </head>
       <body>
         <Navbar />
