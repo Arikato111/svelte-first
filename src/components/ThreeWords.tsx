07d8e2838545ea3189a8e91de6d2a0a83c1b506f
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2024-02-29 16:53:05 +0700

    fix: three words not show in darkmode; add new music

--- src/components/ThreeWords.tsx
+++ src/components/ThreeWords.tsx
@@ -1,10 +1,10 @@
 export default function ThreeWords() {
     return (
-        <div className='h-[200px] flex justify-cjnter items-center'>
+        <div className='h-[200px] flex justify-cjnter items-center dark:text-white'>
             <div className='box my-5 text-2xl lg:text-4xl text-center font-bold'>
                 <div className='md:flex justify-center'>
-                    <div className='p-3 md:border-r-4 border-b-4 lg:border-b-0  border-black'>Learning</div>
-                    <div className='p-3 md:border-r-4 border-b-4 lg:border-b-0 border-black'>Creating</div>
+                    <div className='p-3 md:border-r-4 border-b-4 lg:border-b-0  border-black dark:border-white'>Learning</div>
+                    <div className='p-3 md:border-r-4 border-b-4 lg:border-b-0 border-black dark:border-white'>Creating</div>
                     <div className='p-3 '>Development</div>
                 </div>
             </div>
