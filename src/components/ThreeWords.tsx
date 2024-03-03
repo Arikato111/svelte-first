95fbfde119e35b52236df8cb290f4d513a3499ce
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2024-03-03 23:21:48 +0700

    fix threeWords border

--- src/components/ThreeWords.tsx
+++ src/components/ThreeWords.tsx
@@ -3,8 +3,8 @@ export default function ThreeWords() {
         <div className='h-[200px] flex justify-cjnter items-center dark:text-white'>
             <div className='box my-5 text-2xl lg:text-4xl text-center font-bold'>
                 <div className='md:flex justify-center'>
-                    <div className='p-3 md:border-r-4 border-b-4 lg:border-b-0  border-black dark:border-white'>Learning</div>
-                    <div className='p-3 md:border-r-4 border-b-4 lg:border-b-0 border-black dark:border-white'>Creating</div>
+                    <div className='p-3 md:border-r-4 border-b-4 md:border-b-0  border-black dark:border-white'>Learning</div>
+                    <div className='p-3 md:border-r-4 border-b-4 md:border-b-0 border-black dark:border-white'>Creating</div>
                     <div className='p-3 '>Development</div>
                 </div>
             </div>
